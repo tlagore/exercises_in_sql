8ae2799e2b5b91fbeb995ec9f8bb70890b76beb3
Tyrone Lagore <tyronelagore@gmail.com>
2022-01-17 22:26:00 -0800

    part 1

--- part1/query-joins.sql
+++ part1/query-joins.sql
@@ -1,3 +1,7 @@
 -- write here you answer. Use joins or cross-products
 
 
+SELECT COUNT(*) FROM crew AS c
+	JOIN roles AS r ON c.pid = r.pid AND c.id = r.id 
+	WHERE c.crewtype = 'director'
+	AND (r.roletype = 'actor' OR r.roletype = 'actress');
--- part1/query_no_joins.sql
+++ part1/query_no_joins.sql
@@ -1,2 +1,13 @@
 -- write here you answer. Do not use joins or cross-products
 
+WITH directors AS 
+	(SELECT c.id, c.pid FROM crew AS c
+	 WHERE crewtype = 'director'),
+ main_roles AS 
+	 (SELECT r.id, r.pid FROM roles AS r 
+	  WHERE roletype = 'actor' OR roletype = 'actress')
+ SELECT count(*) FROM main_roles AS mr
+ WHERE EXISTS 
+ 	(SELECT id, pid FROM directors AS d
+		WHERE d.pid = mr.pid AND d.id = mr.id
+	);
