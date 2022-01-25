74c6fa0933ff95b22ccfa590ce4beab63ebe2844
Tyrone Lagore <tyronelagore@gmail.com>
2022-01-25 10:18:22 -0800

    p5 query started

--- part5/your_db_query.sql
+++ part5/your_db_query.sql
@@ -1,1 +1,28 @@
 -- write your query to answer your db question here
+
+WITH 
+	age_range_start AS (
+		SELECT generate_series(1976, 2020, 4) AS from_interval
+	), 
+	age_range AS (
+		SELECT from_interval, (from_interval + 3) AS to_interval FROM age_range_start
+	),
+	highest_party_per_year AS
+	(
+		SELECT ei.year, si.state_name, p.party_name, p.party_simplified, MAX(totalvotes)
+		FROM electioninfo AS ei
+		JOIN stateinfo AS si ON si.id = ei.stateid
+		JOIN party AS p ON p.id = ei.partyid
+		GROUP BY ei.year, si.state_name, p.party_name, p.party_simplified
+	),
+	party_wins AS 
+	(
+		SELECT year, party_name, COUNT(*) AS num_wins FROM highest_party_per_year
+		GROUP BY year, party_name
+	)
+	SELECT from_interval, to_interval,
+		(SELECT SUM(totalvotes) FROM electioninfo AS ei
+			WHERE ei.year BETWEEN from_interval AND to_interval) AS total_votes,
+		(SELECT MAX(totalvotes) FROM electioninfo AS ei
+			WHERE ei.year BETWEEN from_interval AND to_interval) AS max_state_votes
+	FROM age_range;
---
+++ specificity.md
@@ -0,0 +1,12 @@
+reltuples = 7,694,460
+
+nullvalues (year) =  0.1117733
+
+null_tuples = 88685.407152
+
+mcv_perc = 0.69599986
+
+
+
+1 - nulltuples - mcv_perc = **histogram_bounds covers 0.19222281 of tuples**
+
